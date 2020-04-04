27e9570f9b8b3b29ac42830f033f8e8d9a196d38
gromovnik1337 <vice.roncevic@gmail.com>
2020-04-04 18:29:44 +0200

    Import ascii pcd (step gauge) and plot

---
+++ convertAndView/build/CMakeFiles/convertAndView.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convertAndView.dir/convertAndView.cpp.o"
+  "convertAndView.pdb"
+  "convertAndView"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/convertAndView.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
